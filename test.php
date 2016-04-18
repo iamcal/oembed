01f808bb2a8d7d5c2fe2a11a54f9e39dd450a790
Cal Henderson <cal@iamcal.com>
2016-04-18 12:57:42 -0700

    switched the CI test over to checking the new data files

--- test.php
+++ test.php
@@ -1,9 +1,20 @@
 <?php
+	$num_files = 0;
+	$num_entries = 0;
 
-$data = yaml_parse_file('providers.yml');
+	$dh = opendir('providers');
+	while (($file = readdir($dh)) !== false){
+		if (preg_match('!\.yml$!', $file)){
+			$partial = yaml_parse_file("providers/$file");
+			if (!$partial || !is_array($partial)){
+				echo "Unable to parse provider file providers/$file\n";
+				exit(1);
+			}
 
-if (!$data) {
-    exit(1);
-} else {
-    exit(0);
-}
+			$num_files++;
+			$num_entries += count($partial);
+		}
+	}
+
+	echo "Loaded $num_files provider files, containing $num_entries entries\n";
+	exit(0);
