4b959b4d3cb80745ee8642ca4cea312bd26234d4
Cal Henderson <cal@iamcal.com>
2022-01-05 15:43:39 +0000

    updated schemes test, but still disabled

--- test.php
+++ test.php
@@ -58,13 +58,15 @@
 				}
 				foreach ($def['endpoints'] as $endpoint){
 
-				# NOTE: this is disabled because it counts e.g. wordpress as broken.
-				# maybe it is?
+					# this test is currently disabled because three providers fail it.
+					# i _believe_ they should be deleted, as they are of no use to consumers.
 
-				#	if (!isset($endpoint['schemes']) || !count($endpoint['schemes'])){
-				#		echo "Endpoint without schemes found in provider file providers/$file\n";
-				#		print_r($endpoint);
-				#		exit(1);
+				#	if (!isset($endpoint['discovery']) || $endpoint['discovery'] === 0){
+				#		if (!isset($endpoint['schemes']) || !count($endpoint['schemes'])){
+				#			echo "Endpoint without schemes or discovery found in provider file providers/$file\n";
+				#			print_r($endpoint);
+				#			exit(1);
+				#		}
 				#	}
 
 					if (!isset($endpoint['url'])){
