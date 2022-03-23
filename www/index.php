d58d57345b641b3254f8642af44f8c80767010ba
Cal Henderson <cal@iamcal.com>
2022-03-23 23:43:07 +0000

    added missing meta description

--- www/index.php
+++ www/index.php
@@ -39,6 +39,7 @@
 <head>
 <title>oEmbed</title>
 <meta name="viewport" content="width=device-width">
+<meta name="description" content="The oEmbed spec">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
