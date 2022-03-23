0dc1535874ad2f63498b0379589d61bc5c7403d6
Cal Henderson <cal@iamcal.com>
2022-03-23 23:40:31 +0000

    mobile perf optimization

--- www/index.php
+++ www/index.php
@@ -37,6 +37,7 @@
 <html lang="en">
 <head>
 <title>oEmbed</title>
+<meta name="viewport" content="width=device-width">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
